b3fe8b3255c48f7730295338d6e48bc6a348e0d8
Uri Arev <uri.arev@gmail.com>
2020-09-17 09:44:15 +0300

    Added the option to read/view JSON/answers

---
+++ api/read.py
@@ -0,0 +1,15 @@
+import os
+
+def read(): # Only when the file is accepted as open.
+    i = 0
+    if os.path.isfile("data.json"):
+        with open("data.json", "r") as f:
+            for line in f:
+                i += 1
+                print("{} | {}".format(i, line))
+    else:
+        print("File not found. Create one using the API with \"python3 api.py\" and make sure the server is running.")
+
+
+if __name__ == "__main__":
+    read()
