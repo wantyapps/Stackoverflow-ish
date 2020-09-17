c3ee80372e70d4104db01d8b66acc82dd97141c9
Uri Arev <uri.arev@gmail.com>
2020-09-17 21:34:47 +0300

    Adding the DEBUG mode, with the rundebug.go compatibility

--- api/api.py
+++ api/api.py
@@ -1,19 +1,25 @@
 import requests
 import json
 import os
+import sys
 
-try:    
-    response = requests.get("http://localhost:5000/api", headers = { "username": "wantyapps", "password": "pass123" } )
-except requests.exceptions.ConnectionError:
-    response = False
-    print("Connection Error.")
-if response:    
-    data = json.loads(response.text)
-    if os.path.isfile("data.json"):
-        with open("data.json", "a") as f:
-            f.write("\n" + str(data).replace("'", '"'))
-    else:
-        with open("data.json", "a") as f:
-            f.write(str(data).replace("'", '"'))
+if len(sys.argv) == 2:
+    if sys.argv[1] == "--debug" or sys.argv[1] == "-d":
+        print("DEBUG")
+else:
+
+    try:    
+        response = requests.get("http://localhost:5000/api", headers = { "username": "wantyapps", "password": "pass123" } )
+    except requests.exceptions.ConnectionError:
+        response = False
+        print("Connection Error.")
+    if response:    
+        data = json.loads(response.text)
+        if os.path.isfile("data.json"):
+            with open("data.json", "a") as f:
+                f.write("\n" + str(data).replace("'", '"'))
+        else:
+            with open("data.json", "a") as f:
+                f.write(str(data).replace("'", '"'))
 
 # I want a colorprint here of the data...
