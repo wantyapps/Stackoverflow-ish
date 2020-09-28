5b1647011560178b21e6438636a3c321e438c200
Uri Arev <uri.arev@gmail.com>
2020-09-28 18:39:41 +0300

    The first half is done.

--- src/api/api.py
+++ src/api/api.py
@@ -2,7 +2,7 @@ import requests
 import json
 import os
 import sys
-import read
+from read import read
 
 if len(sys.argv) == 3:
     if sys.argv[1] == "--debug" or sys.argv[1] == "-d":
@@ -14,6 +14,18 @@ if len(sys.argv) == 3:
             print("[\033[91m!\033[0m] [API] Connection error")
         if response:
             print("[\033[92m*\033[0m] Response: {}".format(response.text).replace("\n", ""))
+            while True:    
+                print("[\033[92m*\033[0m] CLI MODE ENABLED")
+                readatain = input("Read JSON Data from database? [y/n]: ")
+                if readatain == "y":
+                    read.read()
+                    break
+                elif readatain == "n":
+                    print("[\033[92m*\033[0m] ACCEPTED")
+                    break
+                else:
+                    print("[\033[91m!\033[0m] NOT ACCEPTED")
+
 else:
 
     try:    
